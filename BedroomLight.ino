d0efc6f7558fabd6bc4a16932ffe4f83f2e6698f
Benjamin Boxler <benjaminboxler@gmail.com>
2017-08-14 12:17:09 +0100

    MQTT basics

--- BedroomLight.ino
+++ BedroomLight.ino
@@ -6,6 +6,7 @@
 #include <PubSubClient.h>
 
 enum wifiMode {AP, NORMAL};
+enum mode { MOOD, STROBE, SOLID, POLICE, OFF };
 
 #define MQTT_SERVER "192.168.0.100"
 char ssid[50];
@@ -20,6 +21,14 @@ ESP8266WebServer server(80);
 int ledLength = 2;
 int ledPin = 14;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(ledLength, ledPin);
+
+char* moodTopic = "bedroom/#";
+void callback(char* topic, byte* payload, unsigned int length);
+
+WiFiClient wifiClient;
+PubSubClient client(MQTT_SERVER, 1883, callback, wifiClient);
+
+int colorMode = MOOD;
 
 void setup() {
 
@@ -132,7 +141,102 @@ void setupServer() {
   server.begin();
 }
 
+void lightsOut() {
+	for (int i = 0; i <= ledLength; i++) {
+		pixels.setPixelColor(i, 0,0,0);
+		pixels.show();
+	};
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+	//convert topic to string to make it easier to work with
+	String topicStr = topic;
+
+	//Print out some debugging info
+	Serial.println("Callback update.");
+	Serial.print("Topic: ");
+	Serial.println(topicStr);
+
+	Serial.print("Payload: ");
+	Serial.println((char*)payload);
+
+	//if (topicstr == "bedroom/strobelight") {
+	//	//turn the light on if the payload is '1' and publish to the mqtt server a confirmation message
+	//	if (char(payload[0]) == '1') {
+	//		colormode = strobe;
+	//		client.publish("/test/confirm", "light on");
+	//	}
+
+	//	//turn the light off if the payload is '0' and publish to the mqtt server a confirmation message
+	//	else if (char(payload[0]) == '0') {
+	//		colormode = off;
+	//		lightsout();
+	//		client.publish("/test/confirm", "light off");
+	//	}
+	//}
+
+	//if (topicstr == "bedroom/policelight") {
+	//	//turn the light on if the payload is '1' and publish to the mqtt server a confirmation message
+	//	if (char(payload[0]) == '1') {
+	//		colormode = police;
+	//		client.publish("/test/confirm", "light on");
+	//	}
+
+	//	//turn the light off if the payload is '0' and publish to the mqtt server a confirmation message
+	//	else if (char(payload[0]) == '0') {
+	//		colormode = off;
+	//		lightsout();
+	//		client.publish("/test/confirm", "light off");
+	//	}
+	//}
+
+	if (topicStr == "bedroom/moodlight") {
+		//turn the light on if the payload is '1' and publish to the MQTT server a confirmation message
+		if (char(payload[0]) == '1') {
+			colorMode = MOOD;
+			client.publish("/test/confirm", "Light On");
+		}
+
+		//turn the light off if the payload is '0' and publish to the MQTT server a confirmation message
+		else if (char(payload[0]) == '0') {
+			colorMode = OFF;
+			lightsOut();
+			client.publish("/test/confirm", "Light Off");
+		}
+	}
+
+}
+
+void moodLight() {
+  for(int j=0; j <= 255; j++) {
+    for(int i=0; i < ledLength; i++){
+     pixels.setPixelColor(i,j,0,255-j);
+     delay(10);
+    };
+  };
+  //delay(500);
+  for(int j=0; j <= 255; j++) {
+    for(int i=0; i < ledLength; i++){
+      pixels.setPixelColor(i, 255 - j, j, 0);
+      pixels.show();
+      delay(10);
+    };
+  };
+  //delay(500);
+  for(int j=0; j <= 255; j++) {
+    for(int i=0; i < ledLength; i++){
+		  pixels.setPixelColor(i,0,255-j,j);
+		  pixels.show();
+      delay(10);
+    };
+  };
+  //delay(500);
+}
+
 void loop() {
+  connectWifi();
+  client.loop();
   server.handleClient();
   if (currentWifiMode == AP) {
     pixels.setPixelColor(0,0,0,100);
@@ -142,28 +246,90 @@ void loop() {
     pixels.show();
     delay(200);
   }
+
+
+  switch (colorMode) {
+  case MOOD:
+    pixels.setPixelColor(0, 250, 0, 250);
+    pixels.show();
+	  //moodLight();
+	  break;
+  case STROBE:
+	  //strobeLight();
+	  break;
+  case POLICE:
+	  //policeLight();
+	  break;
+  case SOLID:
+	  break;
+  default:
+	  lightsOut();
+  };
 }
 
-bool connectWifi(){
-  Serial.print("Connecting to ");
-  Serial.println((char*)ssid);
-  WiFi.begin(ssid,passwd);
+void connectWifi(){
+  
   timer = 0;
   while (WiFi.status() != WL_CONNECTED && timer < 10000) {
-      pixels.setPixelColor(0,255,0,0);
-      pixels.show();
-      delay(500);
-      Serial.print(".");
-    }
-    if (WiFi.status() == WL_CONNECTED) {
-      pixels.setPixelColor(0,0,255,0);
-      pixels.show();
-      return true;
-    } else {
-      WiFi.disconnect();
-      setupAP();
-      return false;
-    }
+    Serial.print("Connecting to ");
+    Serial.println((char*)ssid);
+    WiFi.begin(ssid, passwd);
+    pixels.setPixelColor(0,255,0,0);
+    pixels.show();
+    delay(500);
+    Serial.print(".");
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.disconnect();
+    setupAP();
+	return;
+  }
+
+	if (WiFi.status() == WL_CONNECTED) {
+		// Loop until we're reconnected to the MQTT server
+		while (!client.connected()) {
+			Serial.print("Attempting MQTT connection...");
+
+			// Generate client name based on MAC address and last 8 bits of microsecond counter
+			String clientName;
+			clientName += "esp8266-";
+			uint8_t mac[6];
+			WiFi.macAddress(mac);
+			clientName += macToStr(mac);
+
+			//if connected, subscribe to the topic(s) we want to be notified about
+			if (client.connect((char*)clientName.c_str(), "esp8266", "elephant")) {
+				Serial.println("\tMTQQ Connected");
+				if (client.subscribe(moodTopic)) {
+
+					Serial.println("All Channels connected");
+
+					// WS2812b green status light 
+					lightsOut();
+					pixels.setPixelColor(0, 0, 255, 0);
+					pixels.show();
+				}
+			}
+
+			//otherwise print failed for debugging
+			else { Serial.println("\tFailed."); abort(); }
+		}
+	}
+}
+
+String macToStr(const uint8_t* mac) {
+
+	String result;
+
+	for (int i = 0; i < 6; ++i) {
+		result += String(mac[i], 16);
+
+		if (i < 5) {
+			result += ':';
+		}
+	}
+
+	return result;
 }
 
 void setupAP() {
